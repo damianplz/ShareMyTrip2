b887a08e201f678a7af311da08df356e1f63a1d9
roberto fernandez diaz <robertofd1995@gmail.com>
2016-04-05 20:08:45 +0200

    Mas test añadidos

--- sdi-test-22/src/com/sdi/tests/Tests/SDI2_Tests.java
+++ sdi-test-22/src/com/sdi/tests/Tests/SDI2_Tests.java
@@ -1,10 +1,14 @@
 package com.sdi.tests.Tests;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import net.sf.cglib.proxy.Factory;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
@@ -13,10 +17,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import com.google.common.util.concurrent.Service;
 import com.sdi.infrastructure.Factories;
+import com.sdi.model.Trip;
 import com.sdi.model.User;
+import com.sdi.persistence.TripDao;
 import com.sdi.persistence.UserDao;
 import com.sdi.tests.pageobjects.PO_AltaForm;
 import com.sdi.tests.pageobjects.PO_LoginForm;
@@ -30,11 +37,13 @@ public class SDI2_Tests {
 
 	private final static String EXTERNO = "http://localhost:8180/sdi2-22/";
 	private final static String INTERNO="http://localhost:8280/sdi2-22/";
+	private String baseUrl;
 
 	WebDriver driver;
 	List<WebElement> elementos = null;
 	String selectedServer;
 
+	
 	public SDI2_Tests() {
 		selectedServer = INTERNO;
 	}
@@ -43,6 +52,11 @@ public class SDI2_Tests {
 	public void run() {
 		driver = new FirefoxDriver();
 		driver.get(selectedServer);
+		
+		
+		//Para el uso de los test con plugin
+		baseUrl = "http://localhost:8280/sdi2-22/";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
 	@After
@@ -97,81 +111,118 @@ public class SDI2_Tests {
 
 	}
 
-//	// 3. [IdVal] Identificación de Usuario registrado con datos válidos.
-//	@Test
-//	public void t03_IdVal() {
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
-//		new PO_LoginForm().rellenaFormulario(driver, "usuario1", "usuario1");
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "registradoPrincipal", 10);
-//		SeleniumUtils.textoPresentePagina(driver, "Fernando");
-//	}
-//
-//	// 4. [IdInval] Identificación de usuario registrado con datos inválidos.
-//	@Test
-//	public void t04_IdInval() {
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
-//		new PO_LoginForm().rellenaFormulario(driver, "usuario1", "incorrecto");
-//		SeleniumUtils.EsperaCargaPagina(driver, "text",
-//				"La contraseña no es correcta", 10);
-//		SeleniumUtils.textoPresentePagina(driver,
-//				"La contraseña no es correcta");
-//		new PO_LoginForm().rellenaFormulario(driver, "noExisto", "usuario1");
-//		SeleniumUtils.EsperaCargaPagina(driver, "text", "El usuario no existe",
-//				10);
-//		SeleniumUtils.textoPresentePagina(driver, "El usuario no existe");
-//	}
-//
-//	// 5. [AccInval] Intento de acceso con URL desde un usuario no público
-//	// (no // identificado). Intento de acceso a vistas de acceso privado. -
-//	// PASA
-//	@Test
-//	public void t05_AccInval() {
-//		String inval = selectedServer + "infoCompleta.xhtml";
-//		driver.get(inval);
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
-//		SeleniumUtils.textoPresentePagina(driver,
-//				"Formulario de inicio de sesión");
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
-//		SeleniumUtils.textoPresentePagina(driver, "Inicie sesión");
-//	}
-//
-//	// 6. [RegViajeVal] Registro de un viaje nuevo con datos válidos.
-//	@Test
-//	public void t06_RegViajeVal() {
-//		t03_IdVal();
-//		SeleniumUtils
-//				.EsperaCargaPagina(driver, "id", "registradoPrincipal", 10);
-//		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:opciones",
-//				"form-cabecera:linkRegistrarViaje");
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "viaje", 10);
-//		new PO_RegViajeForm().rellenaFormulario(driver, "addres1", "city1",
-//				"state1", "country1", "35260", "", "", "20/08/2016", "12:00",
-//				"address2", "city2", "state2", "country2", "25036", "2.366",
-//				"-53.6982", "21/08/2016", "01:00", "22:05", "4", "296.85",
-//				"Viaje de prueba", "15/08/2016");
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "tablaViajesParti", 10);
-//		SeleniumUtils.textoPresentePagina(driver,
-//				"Mon Aug 15 22:05:00 CEST 2016");
-//	}
-//
-//	// 7. [RegViajeInVal] Registro de un viaje nuevo con datos inválidos.
-//	@Test
-//	public void t07_RegViajeInVal() {
-//		t03_IdVal();
-//		SeleniumUtils
-//				.EsperaCargaPagina(driver, "id", "registradoPrincipal", 10);
-//		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:opciones",
-//				"form-cabecera:linkRegistrarViaje");
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "viaje", 10);
-//		new PO_RegViajeForm().rellenaFormulario(driver, "addres1", "city1",
-//				"state1", "", "35260", "", "", "20/08/2016", "12:00",
-//				"address2", "city2", "state2", "country2", "25036", "2.366",
-//				"-53.6982", "21/08/2016", "01:00", "22:05", "4", "296.85",
-//				"Viaje de prueba", "23/08/2016");
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "viaje", 10);
-//		SeleniumUtils.textoPresentePagina(driver, "Registre un nuevo viaje");
-//	}
-//
+	// 3. [IdVal] Identificación de Usuario registrado con datos válidos.
+	@Test
+	public void t03_IdVal() {
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		new PO_LoginForm().rellenaFormulario(driver, "user1", "user1");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "menuPerfil", 10);
+		SeleniumUtils.textoPresentePagina(driver, "user1");
+	}
+
+	// 4. [IdInval] Identificación de usuario registrado con datos inválidos.
+	@Test
+	public void t04_IdInval() {
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		new PO_LoginForm().rellenaFormulario(driver, "user1", "pepe");
+		SeleniumUtils.EsperaCargaPaginaTiempo(driver, 5);
+		SeleniumUtils.textoPresentePagina(driver,
+				"Usuario o password erróneos");
+		
+	}
+
+	// 5. [AccInval] Intento de acceso con URL desde un usuario no público
+	// (no // identificado). Intento de acceso a vistas de acceso privado. -
+	// PASA
+	@Test
+	public void t05_AccInval() {
+		String inval = selectedServer + "/restricted/perfil.xhtml";
+		driver.get(inval);
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		SeleniumUtils.textoNoPresentePagina(driver,
+				"Perfil");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		SeleniumUtils.textoPresentePagina(driver, "Formulario de login");
+	}
+	// 6. [RegViajeVal] Registro de un viaje nuevo con datos válidos.
+	@Test
+	public void t06_RegViajeVal() {
+		t03_IdVal();
+		SeleniumUtils
+				.EsperaCargaPagina(driver, "id", "registradoPrincipal", 10);
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:opciones",
+				"form-cabecera:linkRegistrarViaje");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "viaje", 10);
+		new PO_RegViajeForm().rellenaFormulario(driver, "addres1", "city1",
+				"state1", "country1", "35260", "", "", "20/08/2016", "12:00",
+				"address2", "city2", "state2", "country2", "25036", "2.366",
+				"-53.6982", "21/08/2016", "01:00", "22:05", "4", "296.85",
+				"Viaje de prueba", "15/08/2016");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "tablaViajesParti", 10);
+		SeleniumUtils.textoPresentePagina(driver,
+				"Mon Aug 15 22:05:00 CEST 2016");
+	}
+
+	// 7. [RegViajeInVal] Registro de un viaje nuevo con datos inválidos.
+	@Test
+	public void t07_RegViajeInVal() throws InterruptedException {
+		
+		TripDao tripDao = Factories.persistence.newTripDao();
+		
+		int numberOfTrips=tripDao.findAll().size();
+		 
+	    driver.get(baseUrl );
+	    driver.findElement(By.id("form-cuerpo:login")).clear();
+	    driver.findElement(By.id("form-cuerpo:login")).sendKeys("user1");
+	    driver.findElement(By.id("form-cuerpo:password")).clear();
+	    driver.findElement(By.id("form-cuerpo:password")).sendKeys("user1");
+	    driver.findElement(By.id("form-cuerpo:j_idt19")).click();
+	    
+	    
+	    //Rellena el formulario con los datos
+	    SeleniumUtils.EsperaCargaPagina(driver, "id", "form-cuerpo:menuPerfil", 2);
+	    driver.findElement(By.xpath("//a[@id='form-cuerpo:publicarViaje']/span")).click();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:calleS")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:calleS")).sendKeys("test");
+	    driver.findElement(By.xpath("//div[@id='form-cuerpo:crearviaje:listaCiudadesD']/div[3]/table/tbody/tr[4]/td")).click();
+	    driver.findElement(By.xpath("//div[@id='form-cuerpo:crearviaje:listaComunidadesD']/div[3]/table/tbody/tr[6]/td")).click();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:paisS")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:paisS")).sendKeys("test");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:cpS")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:cpS")).sendKeys("12345");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:fechaS")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:fechaS")).sendKeys("30-abr-2016 15:27:36");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:calleL")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:calleL")).sendKeys("test");
+	    driver.findElement(By.xpath("//div[@id='form-cuerpo:crearviaje:listaCiudadesS']/div[3]/table/tbody/tr[5]/td")).click();
+	    driver.findElement(By.xpath("//div[@id='form-cuerpo:crearviaje:listaComunidadesA']/div[3]/table/tbody/tr[3]/td")).click();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:paisL")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:paisL")).sendKeys("test");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:cpL")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:cpL")).sendKeys("54321");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:fechaL")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:fechaL")).sendKeys("30-abr-2016 15:27:36");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:limite")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:limite")).sendKeys("29-abr-2016 15:27:36");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:coste")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:coste")).sendKeys("20");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:paxM")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:paxM")).sendKeys("4");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:paxA")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:paxA")).sendKeys("4");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:comment")).clear();
+	    driver.findElement(By.id("form-cuerpo:crearviaje:comment")).sendKeys("testingTest");
+	    driver.findElement(By.id("form-cuerpo:crearviaje:j_idt80")).click();
+	    
+	    
+	    Thread.sleep(1000);
+	    
+	    //We check that the project has been suscessfully created
+	    Assert.assertTrue(numberOfTrips<tripDao.findAll().size());
+	    //Factories.persistence.newTripDao().delete(trip.getId());
+		
+	}
+
 //	// 8. [EditViajeVal] Edición de viaje existente con datos válidos.
 //	@Test
 //	public void t08_EditViajeVal() {
@@ -195,7 +246,7 @@ public class SDI2_Tests {
 //		SeleniumUtils.textoPresentePagina(driver,
 //				"Sat Oct 15 12:50:00 CEST 2016");
 //	}
-//
+
 //	// 9. [EditViajeInVal] Edición de viaje existente con datos inválidos.
 //	@Test
 //	public void t09_EditViajeInVal() {
@@ -507,50 +558,82 @@ public class SDI2_Tests {
 //						+ "'Excluir')]")).click();
 //		SeleniumUtils.EsperaCargaPaginaNoTexto(driver, "Maria Gonzalez", 10);
 //	}
-//
-//	// 17. [i18N1] Cambio del idioma por defecto a un segundo idioma.
-//	// (Probar algunas vistas)
-//	@Test
-//	public void t17_i18N1() {
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
-//		SeleniumUtils.textoPresentePagina(driver, "Inicie sesión");
-//		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:idioma",
-//				"form-cabecera:linkingles");
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie:inicioGeneral",
-//				10);
-//		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:opciones",
-//				"form-cabecera:linkRegistrarse");
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "registro", 10);
-//		SeleniumUtils.textoPresentePagina(driver, "Register a new account");
-//	}
-//
-//	// 18. [i18N2] Cambio del idioma por defecto a un segundo idioma y
-//	// vuelta al idioma por defecto. (Probar algunas vistas)
-//	@Test
-//	public void t18_i18N2() {
-//		t17_i18N1();
-//		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:idioma",
-//				"form-cabecera:linkespa");
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie:inicioGeneral",
-//				10);
-//		driver.findElement(By.xpath(".//a[@id='form-pie:inicioGeneral']"))
-//				.click();
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "login-form", 10);
-//		SeleniumUtils.textoPresentePagina(driver,
-//				"Formulario de inicio de sesión");
-//		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:opciones",
-//				"form-cabecera:linkRegistrarse");
-//		SeleniumUtils.EsperaCargaPagina(driver, "id", "registro", 10);
-//		SeleniumUtils.textoPresentePagina(driver, "Registre una nueva cuenta");
-//	}
-//
-//	/*
-//	 * // 19. [OpFiltrado] Prueba para el filtrado opcional.
-//	 * 
-//	 * @Test public void t19_OpFiltrado() {
-//	 * 
-//	 * } // 20. [OpOrden] Prueba para la ordenación opcional.
-//	 * 
+
+	// 17. [i18N1] Cambio del idioma por defecto a un segundo idioma.
+	// (Probar algunas vistas)
+	@Test
+	public void t17_i18N1() {
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
+		SeleniumUtils.textoPresentePagina(driver, "Formulario de login");
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:subMenuIdioma",
+				"form-cabecera:linkingles");
+		SeleniumUtils.EsperaCargaPaginaTiempo(driver,10);
+		SeleniumUtils.textoPresentePagina(driver, "Login form");
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:misubmenu1",
+				"form-cabecera:linkCrearUsuario");
+		SeleniumUtils.EsperaCargaPagina(driver, "id", "registro", 10);
+		SeleniumUtils.textoPresentePagina(driver, "User registration");
+	}
+
+	// 18. [i18N2] Cambio del idioma por defecto a un segundo idioma y
+	// vuelta al idioma por defecto. (Probar algunas vistas)
+	@Test
+	public void t18_i18N2() {
+		t17_i18N1();
+		
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:subMenuIdioma",
+				"form-cabecera:linkespa");
+		SeleniumUtils.EsperaCargaPaginaTiempo(driver,5);
+		SeleniumUtils.textoPresentePagina(driver, "Registro de usuario");
+		
+		SeleniumUtils.EsperaCargaPaginaTiempo(driver,5);
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:subMenuIdioma",
+				"form-cabecera:linkingles");
+		SeleniumUtils.EsperaCargaPaginaTiempo(driver,2);
+		SeleniumUtils.textoPresentePagina(driver, "User registration");
+		
+		SeleniumUtils.clickOnButton(driver, "form-pie:linkInicioIndex");
+		SeleniumUtils.EsperaCargaPaginaTiempo(driver, 10);
+		
+		SeleniumUtils.ClickSubopcionMenuHover(driver, "form-cabecera:subMenuIdioma",
+				"form-cabecera:linkespa");
+		SeleniumUtils.EsperaCargaPaginaTiempo(driver,10);
+		SeleniumUtils.textoPresentePagina(driver, "Registro de usuario");
+			
+	}
+
+	
+	 // 19. [OpFiltrado] Prueba para el filtrado opcional.
+	 @Test public void t19_OpFiltrado() throws InterruptedException {
+	 
+		//Vamos a la vista de filtrado-ordenacion-paginacion
+		 	login("user1", "user1");
+		 	SeleniumUtils.clickOnButton(driver, "form-cuerpo:verTodos");
+		 
+			//Esperamos que aparezca el elemento filter			
+			List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "class", "ui-column-filter", 2); 
+			
+			//Pinchamos en segundo filtro (el de año)
+			elementos.get(1).click();
+
+			//Escribimos en el campo tecla a tecla. Eso lo hace builder.sendKeys. (No vale el sendKeys de WebElement)
+			Actions builder = new Actions(driver);	    
+			builder.sendKeys("1984").perform(); // moveToElement(hoverElement).perform();
+
+			//Esperar porque se recargue la página el resultado del filtrado
+			Thread.sleep(1000);
+
+			//COmo los datos son aleatorios es imposible saber lo que
+			//vamos a encontrarnos pero podríamos...
+			//Contar el número de filas resultados del filtrado (POr ejemplo  4)
+			//tr[contains(@class, 'datatable-selectable')]
+			//By busqueda = By.xpath("//tr[contains(@class, 'datatable-selectable')]");
+			//campos = driver.findElements(busqueda);			
+			//assertTrue("Filtrado incorrecto", campos.size()== 4);	
+		 
+	 }
+	  // 20. [OpOrden] Prueba para la ordenación opcional.
+//	 /* 
 //	 * @Test public void t20_OpOrden() {
 //	 * 
 //	 * } // 21. [OpPag] Prueba para la paginación opcional.
@@ -563,12 +646,12 @@ public class SDI2_Tests {
 //	 * 
 //	 * }
 //	 */
-
+	
 	private void login(String user, String pass) {
 		SeleniumUtils.EsperaCargaPagina(driver, "id", "form-pie", 10);
 		new PO_LoginForm().rellenaFormulario(driver, user, pass);
 		SeleniumUtils
-				.EsperaCargaPagina(driver, "id", "registradoPrincipal", 10);
+				.EsperaCargaPagina(driver, "id", "form-cuerpo:menuPerfil", 10);
 	}
 
 }
